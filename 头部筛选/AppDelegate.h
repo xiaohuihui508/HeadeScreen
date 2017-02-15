b6586076480b43290dfcbe77271fa123ea6d10b6
xiaohuihui508 <15136718290@163.com>
2017-02-15 13:52:13 +0800

    Initial Commit

---
+++ 头部筛选/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  头部筛选
+//
+//  Created by mac on 17/2/15.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
